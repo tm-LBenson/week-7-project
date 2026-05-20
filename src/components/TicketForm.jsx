349f8f64cd1cdd991c7aea1957118fbbef3484ac
Lewis Benson <tm.lewisbenson@gmail.com>
2026-05-20 19:37:51 -0400

    Add content for the ticket form

--- src/components/TicketForm.jsx
+++ src/components/TicketForm.jsx
@@ -1,9 +1,79 @@
+import { useState } from "react";
+import { supabase } from "../supabase";
 
+function TicketForm({ onCreateNewTicket }) {
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [priority, setPriority] = useState("");
+  const [category, setCategory] = useState("");
 
-function TicketForm() {
+  async function handleSubmit(e) {
+    e.preventDefault();
+    const newTicket = {
+      title,
+      description,
+      priority,
+      category,
+      status: "Open",
+    };
+    await supabase.from("tickets").insert(newTicket);
+  
+    setTitle("");
+    setDescription("");
+    setPriority("");
+    setCategory("");
+    onCreateNewTicket();
+  }
+
   return (
-    <div>TicketForm</div>
-  )
+    <form onSubmit={handleSubmit}>
+      <label>
+        Title
+        <input
+          value={title}
+          type="text"
+          name="title"
+          id="title"
+          onChange={(e) => setTitle(e.target.value)}
+        />
+      </label>
+      <label>
+        Description
+        <input
+          value={description}
+          type="text"
+          name="description"
+          id="description"
+          onChange={(e) => setDescription(e.target.value)}
+        />
+      </label>
+      <label>
+        Priority
+        <input
+          value={priority}
+          type="text"
+          name="priority"
+          id="priority"
+          onChange={(e) => setPriority(e.target.value)}
+        />
+      </label>
+      <label>
+        Category
+        <select
+          name="category"
+          id="category"
+          value={category}
+          onChange={(e) => setCategory(e.target.value)}
+        >
+          <option value="">Category</option>
+          <option value="Account Access">Account Access</option>
+          <option value="Hardware">Hardware</option>
+          <option value="Devices">Devices</option>
+        </select>
+      </label>
+      <button>Add Ticket</button>
+    </form>
+  );
 }
 
-export default TicketForm
+export default TicketForm;
