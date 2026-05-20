582f093b5e748e760ef77a0b312e8f08acb9c3f9
Lewis Benson <tm.lewisbenson@gmail.com>
2026-05-20 19:38:56 -0400

    Removed tickets logic and passed as props instead

--- src/components/TicketList.jsx
+++ src/components/TicketList.jsx
@@ -1,18 +1,10 @@
-import React, { useEffect, useState } from "react";
+import  { useEffect, useState } from "react";
 
 import TicketCard from "./TicketCard";
 import styles from "./TicketList.module.css";
-import { supabase } from "../supabase";
 
-export default function TicketList() {
-  const [tickets, setTickets] = useState([]);
-  useEffect(() => {
-    async function getTickets() {
-      const { data } = await supabase.from("tickets").select("*");
-      setTickets(data);
-    }
-    getTickets();
-  }, []);
+export default function TicketList({tickets}) {
+
   return (
     <div>
       <h2 className={styles.center}>Tickets</h2>
